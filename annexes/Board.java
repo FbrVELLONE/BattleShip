3b4249a736cf5a571ed67f77ea446411ab398f43
Fabricio V <fabriciovellone@gmail.com>
2020-02-04 10:07:33 +0100

    first commit

---
+++ annexes/Board.java
@@ -0,0 +1,10 @@
+public class Board{
+    String nom;
+    Character table[][];
+    boolean frappes[][];
+
+    public Board(String nom, int x, int y){
+        this.nom = nom;
+        this.table[x][y];
+    }
+};
